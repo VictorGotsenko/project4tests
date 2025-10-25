afde9be778a20cf399812e9252e61b8af7b76dc7
VictorGotsenko <victor.gotsenko@gmail.com>
2025-10-25 17:26:06 +0500

    Mod param in Dockerfile.Add ENV profile

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /
 
 COPY / .
 
+ENV SPRING_PROFILES_ACTIVE=dev
+
 RUN ./gradlew --no-daemon clean build
 
 CMD ["java", "-jar", "build/libs/project4tests-0.0.1-SNAPSHOT.jar"]
--- src/main/java/hexlet/code/model/User.java
+++ src/main/java/hexlet/code/model/User.java
@@ -1,17 +1,20 @@
 package hexlet.code.model;
 
+import static jakarta.persistence.GenerationType.IDENTITY;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+
+
 
 @Entity
 @Table(name = "users")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
     private String firstName;
